d16a586ac85fff341fe7bee6b529e0cec9a2c377
Kofifus <mikagenic@gmail.com>
2023-04-06 06:12:32 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-import { app } from "https://cdn.skypack.dev/hyperapp";
+import { app } from "https://cdn.skypack.dev/hyperapp"
 
-export const getSubComponent = (elem, name) => elem.shadowRoot.querySelector(name);
-export const getSubComponents = (elem, name) => Object.values(elem.shadowRoot.querySelector(name));
-
-export const componentApp = (name, fn) => {
+export default (name, fn) => { 
   
   customElements.define(name, class extends HTMLElement {
-    #dispatch;
+    #dispatch
     
     constructor () {
       super() 
@@ -14,8 +11,8 @@ export const componentApp = (name, fn) => {
     }
     
     connectedCallback () { 
-      const elem = this;
-      const opts = fn(elem);
+      const elem = this
+      const opts = fn(elem)
       
       // to each event in elem.events add an effect trigering that event 
       Object.keys(elem.events || {}).forEach(n => {
@@ -34,19 +31,19 @@ export const componentApp = (name, fn) => {
         }
         
         const stateChangeMw  = stateMiddleware(state => {
-          if (state===undefined) return;
+          if (state===undefined) return
           const options = { detail: state===undefined ? undefined : (opts.externalState ? opts.externalState(state) : state) }
-          elem.dispatchEvent(new CustomEvent(`stateChange`, options));
-          return state;
+          elem.dispatchEvent(new CustomEvent(`stateChange`, options))
+          return state
         })
         
         const orgDispatch = opts.dispatch
-        opts.dispatch = orgDispatch ? d => stateChangeMw(orgDispatch(d)) :  d => stateChangeMw(d);
+        opts.dispatch = orgDispatch ? d => stateChangeMw(orgDispatch(d)) :  d => stateChangeMw(d)
       }
       
       // prevent view renders after disconnection
       if (opts.view) {
-        const viewFn = opts.view;
+        const viewFn = opts.view
         opts.view = s => s===undefined ? '' : viewFn(s)
       }
       
@@ -72,7 +69,7 @@ export const componentApp = (name, fn) => {
       }
       
       // start app and save disaptch fn
-      this.#dispatch = app({ init: opts.init, view: opts.view, subscriptions: opts.subscriptions, node: elem.shadowRoot.firstChild , dispatch: opts.dispatch }); 
+      this.#dispatch = app({ init: opts.init, view: opts.view, subscriptions: opts.subscriptions, node: elem.shadowRoot.firstChild , dispatch: opts.dispatch }) 
       
       // map any entry in methods to an elem property dispatching an action
       if (opts.methods) { 
@@ -80,8 +77,8 @@ export const componentApp = (name, fn) => {
           Object.entries(opts.methods).map(
             ([name, action]) => [name, () => this.#dispatch(action)]
           )
-        );
-        Object.assign(elem, methodsProps);
+        )
+        Object.assign(elem, methodsProps)
       }
       
       // map any entry in props to a property where set dispatches an action 
